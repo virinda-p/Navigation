c897e4323aa1e2c9fbdfdbf1ac324aa77dfb7026
Virinda Popli <poplivirinda0@gmail.com>
2025-07-10 10:01:13 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Scholar on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
